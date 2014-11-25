a68b4c1ba86721b44de7e658d72214bc68a6356a
Keylor Mora <keylor@orbelink.com>
2014-11-25 17:58:04 -0600

    producos

--- sites/all/themes/hidrotica/templates/field/field--field-marcas.tpl.php
+++ sites/all/themes/hidrotica/templates/field/field--field-marcas.tpl.php
@@ -10,12 +10,25 @@ $url = "";
 		$url = $items[$i]['field_enlace_banner']['#items'][0]['url'];
 	}
 
-	$imagen .= "<li>
+	if($i==(count($items)-1)){
+		$last="logo-marca-last";
+	}else{
+		$last="";
+	}
+
+	if($i==0){
+		$first="logo-marca-first";
+	}else{
+		$first=""; 
+	}
+
+
+	$imagen .= "<li class=\"logo-marca logo-marca-".($i+1)." ".$first." ".$last."\">
 	     		 <a href=\"".$url."\"><img src=\"/sites/default/files/imagen_marca/".$items[$i]['field_image']['#items'][0]['filename']."\" /></a>
 	 		   </li>";
 } ?>
 	<div class="marcas">
-		<p>NUESTRAS MARCAS</p>
+		<h3>NUESTRAS MARCAS</h3>
 	  <ul class="listado-marcas">
 	    <?php print $imagen ?>
 	  </ul>
